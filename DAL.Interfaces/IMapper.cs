cf8cc8d4c52d1cf255a1a6ea71940d5d2f6b03ef
Vadim <shienenko@gmail.com>
2016-02-20 22:04:34 +0300

    Add interfaces mapper and entity. Edit repository

---
+++ DAL.Interfaces/IMapper.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace DAL.Interfaces
+{
+    public interface IMapper<TEntity, TDal>
+    {
+        TEntity ToEntity(TDal item);
+        TDal ToDal(TEntity item);
+        IEnumerable<TDal> ToDalCollection(IEnumerable<TEntity> entity);
+        void CopyFields(TDal dalEntity, TEntity entity);
+    }
+}
