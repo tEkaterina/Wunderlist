2c1dcc8e5d5cc54661b931959a8a7dbb4bdc6758
Vadim <shienenko@gmail.com>
2016-02-22 23:16:50 +0300

    Delete InRequestScope

--- DependencyResolver/Resolver.cs
+++ DependencyResolver/Resolver.cs
@@ -19,17 +19,17 @@ namespace DependencyResolver
         {
             kernel.Bind<DbContext>().To<EntityContext>().InRequestScope();
 
-            kernel.Bind<IRepository<UserDal>>().To<Repository<User, UserDal>>().InRequestScope();
-            kernel.Bind<IRepository<TaskDal>>().To<Repository<Task, TaskDal>>().InRequestScope();
-            kernel.Bind<IRepository<TaskStatusDal>>().To<Repository<TaskStatus, TaskStatusDal>>().InRequestScope();
-            kernel.Bind<IRepository<ToDoListDal>>().To<Repository<ToDoList, ToDoListDal>>().InRequestScope();
+            kernel.Bind<IRepository<UserDal>>().To<Repository<User, UserDal>>();
+            kernel.Bind<IRepository<TaskDal>>().To<Repository<Task, TaskDal>>();
+            kernel.Bind<IRepository<TaskStatusDal>>().To<Repository<TaskStatus, TaskStatusDal>>();
+            kernel.Bind<IRepository<ToDoListDal>>().To<Repository<ToDoList, ToDoListDal>>();
 
             kernel.Bind<IMapper<User, UserDal>>().To<UserMapper>().InSingletonScope();
             //TODO: mappers for other
 
             kernel.Bind<IUnitOfWork>().To<UnitOfWork>().InRequestScope();
 
-            kernel.Bind<IUserService>().To<UserService>().InRequestScope();
+            kernel.Bind<IUserService>().To<UserService>();
             //TODO: bind other services
         }
     }
