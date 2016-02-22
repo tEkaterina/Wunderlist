da027ca01f30592533a39939a9c46c374aef73db
Ekaterina.Tselishcheva <ekaterina.tselishcheva@gmail.com>
2016-02-22 23:35:00 +0300

    Remove request scopes for repositories and services.

--- DependencyResolver/Resolver.cs
+++ DependencyResolver/Resolver.cs
@@ -18,18 +18,17 @@ namespace DependencyResolver
         public static void Configure(this IKernel kernel)
         {
             kernel.Bind<DbContext>().To<EntityContext>().InRequestScope();
+            kernel.Bind<IUnitOfWork>().To<UnitOfWork>().InRequestScope();
 
-            kernel.Bind<IRepository<UserDal>>().To<Repository<User, UserDal>>().InRequestScope();
-            kernel.Bind<IRepository<TaskDal>>().To<Repository<Task, TaskDal>>().InRequestScope();
-            kernel.Bind<IRepository<TaskStatusDal>>().To<Repository<TaskStatus, TaskStatusDal>>().InRequestScope();
-            kernel.Bind<IRepository<ToDoListDal>>().To<Repository<ToDoList, ToDoListDal>>().InRequestScope();
+            kernel.Bind<IRepository<UserDal>>().To<Repository<User, UserDal>>();
+            kernel.Bind<IRepository<TaskDal>>().To<Repository<Task, TaskDal>>();
+            kernel.Bind<IRepository<TaskStatusDal>>().To<Repository<TaskStatus, TaskStatusDal>>();
+            kernel.Bind<IRepository<ToDoListDal>>().To<Repository<ToDoList, ToDoListDal>>();
 
             kernel.Bind<IMapper<User, UserDal>>().To<UserMapper>().InSingletonScope();
             //TODO: mappers for other
-
-            kernel.Bind<IUnitOfWork>().To<UnitOfWork>().InRequestScope();
-
-            kernel.Bind<IUserService>().To<UserService>().InRequestScope();
+            
+            kernel.Bind<IUserService>().To<UserService>();
             //TODO: bind other services
         }
     }
