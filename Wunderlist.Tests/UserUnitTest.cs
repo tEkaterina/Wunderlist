c44298bb3245f2d2edbf540f86182f468af0fdd0
Vadim <shienenko@gmail.com>
2016-02-29 17:16:50 +0300

    Install Moq framework

---
+++ Wunderlist.Tests/UserUnitTest.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Wunderlist.DataAccess.Interfaces;
+using Wunderlist.DataAccess.Interfaces.Entities;
+
+namespace Wunderlist.Tests
+{
+    [TestClass]
+    public class UserUnitTest
+    {
+        [TestMethod]
+        public void CreateUserTest()
+        {
+            // Arrange
+            //var mock = new Mock<IRepository<UserDalEntity>>();
+            //mock.Setup(a => a.GetAll()).Returns(new List<UserDalEntity>()
+            //{
+            //    new UserDalEntity(1),
+            //    new UserDalEntity(2)
+            //});
+        }
+    }
+}
