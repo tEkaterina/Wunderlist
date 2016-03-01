2de042a460cca3640d3f8bf6ef2301a5b1e4255b
Vadim <shienenko@gmail.com>
2016-03-01 17:40:14 +0300

    Add ninject dependency resolver

---
+++ Wunderlist.WebUI/Infrastructure/NinjectDependencyResolver.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+using Ninject;
+using Wunderlist.DependencyResolver;
+
+namespace Wunderlist.WebUI.Infrastructure
+{
+    public class NinjectDependencyResolver : IDependencyResolver
+    {
+        private IKernel kernel;
+
+        public NinjectDependencyResolver(IKernel kernel)
+        {
+            this.kernel = kernel;
+            kernel.Configure();
+        }
+        public object GetService(Type serviceType)
+        {
+            return kernel.TryGet(serviceType);
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return kernel.GetAll(serviceType);
+        }
+    }
+}
