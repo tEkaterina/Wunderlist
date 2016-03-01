e19e4bd701333e7044764bc7b92eebd9677f4da0
Vadim <shienenko@gmail.com>
2016-03-01 17:55:42 +0300

    Fix error in resolver

--- Wunderlist.WebUI/App_Start/NinjectWebCommon.cs
+++ Wunderlist.WebUI/App_Start/NinjectWebCommon.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Web;
-using System.Web.Mvc;
 using Microsoft.Web.Infrastructure.DynamicModuleHelper;
 using Ninject;
 using Ninject.Web.Common;
@@ -62,7 +61,7 @@ namespace Wunderlist.WebUI
         /// <param name="kernel">The kernel.</param>
         private static void RegisterServices(IKernel kernel)
         {
-            DependencyResolver.SetResolver(new NinjectDependencyResolver(kernel));
+            System.Web.Mvc.DependencyResolver.SetResolver(new NinjectDependencyResolver(kernel));
         }        
     }
 }
