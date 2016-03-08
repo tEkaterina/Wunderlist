12f233a46a85db27be948f02d43edee3e03985c9
tEkaterina <ekaterina.tselishcheva@gmail.com>
2016-03-08 22:54:21 +0300

    Add "Lato" font.

---
+++ Wunderlist.WebUI/Models/RegistrationUserModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Wunderlist.WebUI.Models
+{
+    public class RegistrationUserModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+    }
+}
