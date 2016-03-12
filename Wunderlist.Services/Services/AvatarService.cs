969766ad0e86c558bd7e7d6b140857cc35ab7ed9
tEkaterina <ekaterina.tselishcheva@gmail.com>
2016-03-12 22:43:06 +0300

    Create avatar service.

---
+++ Wunderlist.Services/Services/AvatarService.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Wunderlist.DataAccess.Interfaces;
+using Wunderlist.DataAccess.Interfaces.Entities;
+using Wunderlist.Services.Interfaces.Entities;
+using Wunderlist.Services.Interfaces.Services;
+using Wunderlist.Services.Mapper;
+
+namespace Wunderlist.Services.Services
+{
+    public class AvatarService : IAvatarService
+    {
+        private readonly IRepository<AvatarDalEntity> _avatarRepository;
+        private readonly IUnitOfWork _uow;
+
+        public AvatarService(IRepository<AvatarDalEntity> avatarRepository, IUnitOfWork uow)
+        {
+            _avatarRepository = avatarRepository;
+            _uow = uow;
+        }
+
+        public void Create(AvatarServiceEntity avatar)
+        {
+            if (avatar == null)
+                throw new ArgumentNullException(nameof(avatar));
+
+            _avatarRepository.Create(avatar.ToDalEntity());
+            _uow.Commit();
+        }
+
+        public void Delete(int id)
+        {
+            if (id <= 0)
+                throw new ArgumentException("The avatar id must be greater than zero.", nameof(id));
+
+            var avatar = _avatarRepository.GetById(id);
+            if (avatar != null)
+            {
+                _avatarRepository.Delete(avatar);
+                _uow.Commit();
+            }
+        }
+
+        public AvatarServiceEntity GetByUserId(int id)
+        {
+            return _avatarRepository
+                .GetById(id)
+                ?.ToServiceEntity();
+        }
+    }
+}
