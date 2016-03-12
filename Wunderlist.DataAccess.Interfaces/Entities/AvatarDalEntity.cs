13aac4e940eb42fa8ac8367c61a99cff4a27f4fa
tEkaterina <ekaterina.tselishcheva@gmail.com>
2016-03-12 22:46:48 +0300

    Remove unused library references.

--- Wunderlist.DataAccess.Interfaces/Entities/AvatarDalEntity.cs
+++ Wunderlist.DataAccess.Interfaces/Entities/AvatarDalEntity.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Wunderlist.DataAccess.Interfaces.Entities
+﻿namespace Wunderlist.DataAccess.Interfaces.Entities
 {
     public class AvatarDalEntity: IDalEntity
     {
