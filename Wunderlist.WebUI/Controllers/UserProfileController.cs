e6ee083cb34777eda96140d11b008684cb025a8f
tEkaterina <ekaterina.tselishcheva@gmail.com>
2016-03-13 22:11:37 +0300

    Add avatar to user info view.

--- Wunderlist.WebUI/Controllers/UserProfileController.cs
+++ Wunderlist.WebUI/Controllers/UserProfileController.cs
@@ -4,24 +4,33 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Wunderlist.Services.Interfaces.Services;
+using Wunderlist.WebUI.Models;
 
 namespace Wunderlist.WebUI.Controllers
 {
     public class UserProfileController : Controller
     {
         private readonly IUserService _userService;
+        private readonly IAvatarService _avatarService;
 
-        public UserProfileController(IUserService userService)
+        public UserProfileController(IUserService userService, IAvatarService avatarService)
         {
             _userService = userService;
+            _avatarService = avatarService;
         }
-
-        // GET: UserProfile
-        [HttpGet]
+        
         public ActionResult GetUserInfo()
         {
             string userEmail = HttpContext.User.Identity.Name;
-            var userProfile = _userService.GetUserEntity(userEmail);
+            var user = _userService.GetUserEntity(userEmail);
+            var avatar = _avatarService.GetByUserId(user.Id);
+
+            var userProfile = new UserProfileModel()
+            {
+                Name = user.Name,
+                Avatar = Convert.ToBase64String(avatar.Image),
+            };
+
             return Json(userProfile, JsonRequestBehavior.AllowGet);
         }
     }
