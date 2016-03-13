e790b6c3bf103c7cba16769837a9649549fe3809
tEkaterina <ekaterina.tselishcheva@gmail.com>
2016-03-13 23:55:31 +0300

    Fix error with user avatar on user info panel. Add user menu arrow button.

--- Wunderlist.WebUI/Controllers/UserProfileController.cs
+++ Wunderlist.WebUI/Controllers/UserProfileController.cs
@@ -1,9 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
+using Wunderlist.Services.Interfaces.Entities;
 using Wunderlist.Services.Interfaces.Services;
+using Wunderlist.WebUI.Infrastructure;
 using Wunderlist.WebUI.Models;
 
 namespace Wunderlist.WebUI.Controllers
@@ -18,12 +17,20 @@ namespace Wunderlist.WebUI.Controllers
             _userService = userService;
             _avatarService = avatarService;
         }
-        
+
+        [HttpGet]
         public ActionResult GetUserInfo()
         {
             string userEmail = HttpContext.User.Identity.Name;
             var user = _userService.GetUserEntity(userEmail);
             var avatar = _avatarService.GetByUserId(user.Id);
+            if (avatar == null)
+            {
+                avatar = new AvatarServiceEntity()
+                {
+                    Image = AvatarCreator.Get(user.Name),
+                };
+            }
 
             var userProfile = new UserProfileModel()
             {
