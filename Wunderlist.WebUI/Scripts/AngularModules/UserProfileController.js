d5253d1e4e42f448690c03f12a8e2cee1ed83bd5
tEkaterina <ekaterina.tselishcheva@gmail.com>
2016-03-15 21:21:11 +0300

    Fix error with UserProfileController file.

---
+++ Wunderlist.WebUI/Scripts/AngularModules/UserProfileController.js
@@ -0,0 +1,55 @@
+﻿var app = angular.module('WunderlistModule');
+
+app.controller('userProfileController', function($scope, $http) {
+    $scope.loadUserProfile = function() {
+        $http.get("/UserProfile/GetUserInfo")
+            .success(function(userProfile) {
+                $scope.userProfile = userProfile;
+            })
+            .error(function(result) {
+                console.log(result);
+            });
+    }
+
+    $scope.loadUserProfile();
+
+    $scope.logout = function() {
+        $http.put("/User/Logout", {})
+            .success(function(redirectUrl) {
+                window.location.href = redirectUrl;
+            })
+            .error(function(result) {
+                console.log(result);
+            });
+    }
+
+    $scope.loadAvatar = function() {
+        var avatar = document.getElementById('avatarInput').files[0], 
+            reader = new FileReader();
+        reader.onloadend = function(e) {
+            var image = e.target.result;
+            image = image.replace(/.+?base64 *\, *?/g, "");
+
+            $http.put("/UserProfile/ChangeAvatar", { image: image })
+                .success(function(result) {
+                    $scope.loadUserProfile();
+                })
+                .error(function(result) {
+                    console.log(result);
+                });
+        }
+        reader.readAsDataURL(avatar);
+    }
+
+    $scope.saveUserProfileChanges = function() {
+        var newUsername = document.getElementById('username').value;
+        $http.put("/UserProfile/ChangeUsername", { newUsername:  newUsername})
+            .success(function(result) {
+            })
+            .error(function(result) {
+                console.log(result);
+            });
+        $scope.loadUserProfile();
+        window.reload();
+    }
+});
