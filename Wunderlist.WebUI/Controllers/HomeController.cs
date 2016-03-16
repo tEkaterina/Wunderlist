1313a1df1b16a62e5f6a1f906435cf48a46fc7a4
tEkaterina <ekaterina.tselishcheva@gmail.com>
2016-03-16 23:55:28 +0300

    Create index page.

--- Wunderlist.WebUI/Controllers/HomeController.cs
+++ Wunderlist.WebUI/Controllers/HomeController.cs
@@ -7,10 +7,15 @@ using Wunderlist.WebUI.Infrastructure;
 
 namespace Wunderlist.WebUI.Controllers
 {
+    [AllowAnonymous]
     public class HomeController : Controller
     {
         public ActionResult Index()
         {
+            if (HttpContext.User.Identity.IsAuthenticated)
+            {
+                return RedirectToAction("Main", "Main");
+            }
             return View();
         }
     }
